3798a8cb1d9f923408ba2698291bcc7862cb5b01
SteelCoder <91021962+Chirag1309@users.noreply.github.com>
2023-04-06 19:45:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+  var button=this.innerHTML;
+  makeSound(button);
+  animate(button);
+
+});
+}
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  animate(event.key);
+});
+function makeSound(button){
+  switch (button) {
+    case "d":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+    break;
+    case "j":
+      var tom2=new Audio("sounds/tom-2.mp3");
+      tom2.play();
+    break;
+    case "k":
+      var tom3=new Audio("sounds/tom-3.mp3");
+      tom3.play();
+    break;
+    case "l":
+      var tom4=new Audio("sounds/tom-4.mp3");
+      tom4.play();
+    break;
+    case "w":
+      var crash=new Audio("sounds/crash.mp3");
+      crash.play();
+    break;
+    case "a":
+      var kick=new Audio("sounds/kick-bass.mp3");
+      kick.play();
+    break;
+    case "s":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+    break;
+    default:console.log(button);
+  }
+}
+function animate(key){
+  var curr=document.querySelector("."+key);
+  curr.classList.add("pressed");
+  setTimeout(function(){
+    curr.classList.remove("pressed");
+  },100);
+}
